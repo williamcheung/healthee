463376179e9ca49ba43f0a5086516755ebba0842
William Cheung <william.cheung@pb.com>
2024-12-08 16:27:01 -0500

    Show 3 dropdowns instead of 1 to stack the iframe rows.

--- ui.py
+++ ui.py
@@ -18,28 +18,58 @@ def read_root():
         <title>Cohort Visualizations</title>
     </head>
     <body>
-        <h3>Select a Cohort to View Visualizations</h3>
-        <select id="dropdown" onchange="updateIframes()">
-            <option value="">Select Cohort</option>
-            {dropdown}
-        </select>
-        <div style="display: flex; justify-content: space-between; margin-top: 20px;">
-            <iframe id="iframe1" width="33%" height="500px" frameborder="0"></iframe>
-            <iframe id="iframe2" width="33%" height="500px" frameborder="0"></iframe>
-            <iframe id="iframe3" width="33%" height="500px" frameborder="0"></iframe>
+        <h3>Select Cohorts to View Visualizations</h3>
+        <div>
+            <label for="dropdown1">Cohort 1:</label>
+            <select id="dropdown1" onchange="updateIframes(1)">
+                <option value="">Select Cohort</option>
+                {dropdown}
+            </select>
         </div>
+        <div style="display: flex; justify-content: space-between; margin-top: 20px;">
+            <iframe id="iframe1_1" width="33%" height="500px" frameborder="0"></iframe>
+            <iframe id="iframe1_2" width="33%" height="500px" frameborder="0"></iframe>
+            <iframe id="iframe1_3" width="33%" height="500px" frameborder="0"></iframe>
+        </div>
+
+        <div>
+            <label for="dropdown2">Cohort 2:</label>
+            <select id="dropdown2" onchange="updateIframes(2)">
+                <option value="">Select Cohort</option>
+                {dropdown}
+            </select>
+        </div>
+        <div style="display: flex; justify-content: space-between; margin-top: 20px;">
+            <iframe id="iframe2_1" width="33%" height="500px" frameborder="0"></iframe>
+            <iframe id="iframe2_2" width="33%" height="500px" frameborder="0"></iframe>
+            <iframe id="iframe2_3" width="33%" height="500px" frameborder="0"></iframe>
+        </div>
+
+        <div>
+            <label for="dropdown3">Cohort 3:</label>
+            <select id="dropdown3" onchange="updateIframes(3)">
+                <option value="">Select Cohort</option>
+                {dropdown}
+            </select>
+        </div>
+        <div style="display: flex; justify-content: space-between; margin-top: 20px;">
+            <iframe id="iframe3_1" width="33%" height="500px" frameborder="0"></iframe>
+            <iframe id="iframe3_2" width="33%" height="500px" frameborder="0"></iframe>
+            <iframe id="iframe3_3" width="33%" height="500px" frameborder="0"></iframe>
+        </div>
+
         <script>
-            function updateIframes() {{
-                const cohort = document.getElementById('dropdown').value;
+            function updateIframes(cohortNumber) {{
+                const cohort = document.getElementById('dropdown' + cohortNumber).value;
                 if (!cohort) {{
-                    document.getElementById('iframe1').src = '';
-                    document.getElementById('iframe2').src = '';
-                    document.getElementById('iframe3').src = '';
+                    document.getElementById('iframe' + cohortNumber + '_1').src = '';
+                    document.getElementById('iframe' + cohortNumber + '_2').src = '';
+                    document.getElementById('iframe' + cohortNumber + '_3').src = '';
                     return;
                 }}
-                document.getElementById('iframe1').src = `/html_files/member_access_by_state_${{cohort}}.html`;
-                document.getElementById('iframe2').src = `/html_files/service_usage_by_race_${{cohort}}.html`;
-                document.getElementById('iframe3').src = `/html_files/cost_by_race_${{cohort}}.html`;
+                document.getElementById('iframe' + cohortNumber + '_1').src = `/html_files/member_access_by_state_${{cohort}}.html`;
+                document.getElementById('iframe' + cohortNumber + '_2').src = `/html_files/service_usage_by_race_${{cohort}}.html`;
+                document.getElementById('iframe' + cohortNumber + '_3').src = `/html_files/cost_by_race_${{cohort}}.html`;
             }}
         </script>
     </body>
