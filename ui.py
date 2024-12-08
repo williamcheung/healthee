4b083c65f4085aa92a4dac9db6ec4248a563b0b4
William Cheung <william.cheung@pb.com>
2024-12-08 16:57:58 -0500

    Add small gap between dropdowns.

--- ui.py
+++ ui.py
@@ -16,57 +16,74 @@ def read_root():
     <html>
     <head>
         <title>Cohort Visualizations</title>
+        <style>
+            .dropdown-container {{
+                margin-bottom: 10px;  /* Small gap between dropdowns */
+            }}
+        </style>
     </head>
     <body>
         <h3>Select Cohorts to View Visualizations</h3>
-        <div>
-            <label for="dropdown1">Cohort 1:</label>
+
+        <div class="dropdown-container">
+            <label for="dropdown1">Cohort:</label>
             <select id="dropdown1" onchange="updateIframes(1)">
                 <option value="">Select Cohort</option>
                 {dropdown}
             </select>
         </div>
-        <div style="display: flex; justify-content: space-between; margin-top: 20px;">
-            <iframe id="iframe1_1" width="33%" height="500px" frameborder="0"></iframe>
-            <iframe id="iframe1_2" width="33%" height="500px" frameborder="0"></iframe>
-            <iframe id="iframe1_3" width="33%" height="500px" frameborder="0"></iframe>
+        <div id="cohort1_iframes" style="display: none; margin-top: 20px;">
+            <div style="display: flex; justify-content: space-between;">
+                <iframe id="iframe1_1" width="33%" height="500px" frameborder="0"></iframe>
+                <iframe id="iframe1_2" width="33%" height="500px" frameborder="0"></iframe>
+                <iframe id="iframe1_3" width="33%" height="500px" frameborder="0"></iframe>
+            </div>
         </div>
 
-        <div>
-            <label for="dropdown2">Cohort 2:</label>
+        <div class="dropdown-container">
+            <label for="dropdown2">Cohort:</label>
             <select id="dropdown2" onchange="updateIframes(2)">
                 <option value="">Select Cohort</option>
                 {dropdown}
             </select>
         </div>
-        <div style="display: flex; justify-content: space-between; margin-top: 20px;">
-            <iframe id="iframe2_1" width="33%" height="500px" frameborder="0"></iframe>
-            <iframe id="iframe2_2" width="33%" height="500px" frameborder="0"></iframe>
-            <iframe id="iframe2_3" width="33%" height="500px" frameborder="0"></iframe>
+        <div id="cohort2_iframes" style="display: none; margin-top: 20px;">
+            <div style="display: flex; justify-content: space-between;">
+                <iframe id="iframe2_1" width="33%" height="500px" frameborder="0"></iframe>
+                <iframe id="iframe2_2" width="33%" height="500px" frameborder="0"></iframe>
+                <iframe id="iframe2_3" width="33%" height="500px" frameborder="0"></iframe>
+            </div>
         </div>
 
-        <div>
-            <label for="dropdown3">Cohort 3:</label>
+        <div class="dropdown-container">
+            <label for="dropdown3">Cohort:</label>
             <select id="dropdown3" onchange="updateIframes(3)">
                 <option value="">Select Cohort</option>
                 {dropdown}
             </select>
         </div>
-        <div style="display: flex; justify-content: space-between; margin-top: 20px;">
-            <iframe id="iframe3_1" width="33%" height="500px" frameborder="0"></iframe>
-            <iframe id="iframe3_2" width="33%" height="500px" frameborder="0"></iframe>
-            <iframe id="iframe3_3" width="33%" height="500px" frameborder="0"></iframe>
+        <div id="cohort3_iframes" style="display: none; margin-top: 20px;">
+            <div style="display: flex; justify-content: space-between;">
+                <iframe id="iframe3_1" width="33%" height="500px" frameborder="0"></iframe>
+                <iframe id="iframe3_2" width="33%" height="500px" frameborder="0"></iframe>
+                <iframe id="iframe3_3" width="33%" height="500px" frameborder="0"></iframe>
+            </div>
         </div>
 
         <script>
             function updateIframes(cohortNumber) {{
                 const cohort = document.getElementById('dropdown' + cohortNumber).value;
+                const iframeContainer = document.getElementById('cohort' + cohortNumber + '_iframes');
+
                 if (!cohort) {{
+                    iframeContainer.style.display = 'none';  // Hide iframe container
                     document.getElementById('iframe' + cohortNumber + '_1').src = '';
                     document.getElementById('iframe' + cohortNumber + '_2').src = '';
                     document.getElementById('iframe' + cohortNumber + '_3').src = '';
                     return;
                 }}
+
+                iframeContainer.style.display = 'block';  // Show iframe container
                 document.getElementById('iframe' + cohortNumber + '_1').src = `/html_files/member_access_by_state_${{cohort}}.html`;
                 document.getElementById('iframe' + cohortNumber + '_2').src = `/html_files/service_usage_by_race_${{cohort}}.html`;
                 document.getElementById('iframe' + cohortNumber + '_3').src = `/html_files/cost_by_race_${{cohort}}.html`;
